83be7241dfef9e2f07bd257827558e528b69f95b
Lucy Mambré <lucy.mambre@epitech.eu>
2018-01-27 23:54:24 +0100

    powerup

---
+++ GGJ2018/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			PickUp (other);
+		}
+	}
+
+	void PickUp(Collider player) {
+		Player pl = player.GetComponent<Player>();
+		int choice = UnityEngine.Random.Range (0, 2);
+
+		if (choice % 2 == 0) {
+			pl.ScorePoint (-1);
+		} else {
+			pl.ScorePoint (1);
+		}
+	}
+}
