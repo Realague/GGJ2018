4570eb0b661c3fd17018fefd7ab1a28627f3fbcc
Unknown <realague@gmail.com>
2018-01-27 17:07:04 +0100

    randomclips

---
+++ GGJ2018/Assets/Scripts/PlayRandomClips.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playRandomClips : MonoBehaviour
+{
+    private Object[] clipsList;
+
+     [SerializeField]
+    private string directory;
+
+    private void Awake()
+    {
+        //load all the music in the folder specified in parameter\\
+        clipsList = Resources.LoadAll(directory, typeof(AudioClip));
+        GetComponent<AudioSource>().clip = clipsList[0] as AudioClip;
+    }
+
+    private void Start()
+    {
+        GetComponent<AudioSource>().Play();	
+	}
+
+	private void Update()
+    {
+		if (!GetComponent<AudioSource>().isPlaying)
+        {
+            playRandomClip();
+        }
+	}
+
+    private void playRandomClip()
+    {
+        GetComponent<AudioSource>().clip = clipsList[Random.Range(0, clipsList.Length)] as AudioClip;
+        GetComponent<AudioSource>().Play();
+    }
+}
