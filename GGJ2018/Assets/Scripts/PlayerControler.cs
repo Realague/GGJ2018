23ef3a01f73d117bde3895728e5e8923ebc2de91
Yann Probst <yann.probst@epitech.eu>
2018-01-27 18:15:31 +0100

    j'encule nils

---
+++ GGJ2018/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+	private Animator myAnimator;
+	[SerializeField]
+	private float speed = 4f;
+	private bool facingRight;
+	private Rigidbody2D player;
+	[SerializeField]
+	private Transform[] groundPoints;
+	[SerializeField]
+	private float groundRadius;
+	[SerializeField]
+	private LayerMask whatIsGround;
+	private bool isGrounded;
+	private bool jump;
+	[SerializeField]
+	private float jumpForce;
+	private bool slide;
+
+	void Start () {
+		facingRight = true;
+		player = GetComponent<Rigidbody2D> ();
+		myAnimator = GetComponent<Animator> ();
+	}
+
+	void Update() {
+		HandleInput ();
+	}
+
+	void FixedUpdate () {
+		isGrounded = isGrounded2();
+		float horizontal = Input.GetAxis ("Horizontal");
+		HandleMovement (horizontal);
+		Flip (horizontal);
+		ResetValues ();
+	}
+
+	private void HandleInput() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			jump = true;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			slide = true;
+		}
+	}
+
+	private void HandleMovement(float horizontal)
+	{
+		player.velocity = new Vector2 (horizontal * speed, player.velocity.y);
+		myAnimator.SetFloat ("speed", Mathf.Abs(horizontal));
+		if (isGrounded && jump) {
+			isGrounded = false;
+			player.AddForce (new Vector2 (0, jumpForce));
+		}
+		if (slide && !this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("Slide")) {
+			myAnimator.SetBool ("slide", true);
+		} else if (!this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("Slide")) {
+			myAnimator.SetBool ("slide", false);
+		}
+	}
+		
+
+	private void Flip(float horizontal) {
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+			facingRight = !facingRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
+
+	private void ResetValues() {
+		jump = false;
+		slide = false;
+	}
+
+	private bool isGrounded2() {
+		if (player.velocity.y <= 0) {
+			foreach (Transform point in groundPoints) {
+				Collider2D[] colliders = Physics2D.OverlapCircleAll (point.position, groundRadius, whatIsGround);
+				for (int i = 0; i < colliders.Length; i++) {
+					if (colliders[i].gameObject != gameObject) {
+						return (true);
+					}
+				}
+			}
+		}
+	 return (false);
+	}
+}
